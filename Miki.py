0ec9b0bdc5a68a637854dffd2cb332c403a02fe3
hansyahm99 <hansyahm99@gmail.com>
2025-09-02 19:09:31 +0700

    Update Miki.py

--- Miki.py
+++ Miki.py
@@ -1,36 +1,25 @@
-import matplotlib.pyplot as plt
 
-Data = {
-        "Edi Supriyanto"            :1205405,
-        "Annisa Putri Restu"        :2535144,
-        "Riska Nurlita"             :1477195,
-        "Syarla Putri Guna"         :552595,
-        "Yandi Nugraha"             :601647,
-        "Yolanda Oktaviani"         :595048,
-        "Erick Ervan Dewanggga"     :0,
-        "Azizah Rahmawati"          :0,
-        "Aldi Taufik"               :677567,
-        "Ika Juliasari"             :0,
-        "Debora Retima Sihombing"   :1762000,
-        "Romli"                     :750631,
-        "Ridhoi Berkat Zebua"       :0,
-        "Fadilah Damayanti"         :3039655,
-        "Erlangga Hutama"           :596045,
-        "Nur Halim"                 :3259599, 
-        "Adistira Winditya P"       :5869259, 
-        "Axl Wicaksono"             :732792
-        }
+import matplotlib.pyplot as plt
+from datetime import datetime
+import pandas as pd 
+
+df = pd.read_excel("miki2.xlsx")
+df = df.fillna(0)
+df["Repayment_amount"] = df['Repayment_amount'].astype(int)
+Data = dict(zip(df['Collector'],df['Repayment_amount']))
 
 nama = list(Data.keys())
 values = list(Data.values())
 
-plt.figure(figsize=(14, 10))
+plt.figure(figsize=(16, 12))
 
 plt.barh(nama, values, color='orange')
 
-plt.title("Report Daily 30 Agustus 2025")
-plt.xlabel("Payment (in units)")
-plt.ylabel("Name")
+today = datetime.today().strftime('%d %B %Y')
+
+plt.title(f"Report Daily {today}", loc='center')
+plt.xlabel("Repayment_amount (in units)")
+plt.ylabel("Collector")
 
 plt.xlim(0,max(values)* 1.1)
 
@@ -42,57 +31,49 @@ for ninja, samurai in enumerate(values):
 
         if samurai >= threshold:
             plt.text(samurai - (max_val*0.01), ninja, f"Rp {samurai:,}", va='center', fontsize=10, ha='right', color='black')
-        
         else:
             plt.text(samurai + (max_val*0.1), ninja, f"Rp {samurai:,}", va='center', fontsize=12, ha='right', color='black')
 
-plt.subplots_adjust(left=0.3, right=0.95,bottom=0.05)
+plt.subplots_adjust(left=0.35, right=0.95,bottom=0.05)
 plt.tight_layout()
 plt.savefig("grafik_bar_daily_payment.png", dpi=100)
 plt.show()
 
-Tim = {"Hansyah_s2l" : {f"Daily paid": 8547124, "Paid amount": 935512454, "Total amount": 9365024946 }}
+Tim = {"Hansyah_s2l" : {f"Daily paid": 25751587, "Paid amount": 25751587, "Total amount": 2622342443 }}
 
-Monthly = {
-        "Edi Supriyanto"            :9.02,
-        "Annisa Putri Restu"        :11.40,
-        "Riska Nurlita"             :14.64,
-        "Syarla Putri Guna"         :9.99,
-        "Yandi Nugraha"             :9.31,
-        "Yolanda Oktaviani"         :9.36,
-        "Erick Ervan Dewanggga"     :11.02,
-        "Azizah Rahmawati"          :9.61,
-        "Aldi Taufik"               :7.99,
-        "Ika Juliasari"             :10.00,
-        "Debora Retima Sihombing"   :11.44,
-        "Romli"                     :10.17,
-        "Ridhoi Berkat Zebua"       :9.32,
-        "Fadilah Damayanti"         :8.92,
-        "Erlangga Hutama"           :7.28,
-        "Nur Halim"                 :8.08, 
-        "Adistira Winditya P"       :8.47, 
-        "Axl Wicaksono"             :6.53
-        }
+df = pd.read_excel("isel.xlsx")
+df = df.fillna(0)
+df["Pending Amount Recovery"] = df['Pending Amount Recovery'].astype(float)
+Monthly = dict(zip(df['Collector'],df['Pending Amount Recovery']))
+
 
 bulan = list(Monthly.keys())
 hasil = list(Monthly.values())
 
-plt.figure(figsize=(14, 10))
+plt.figure(figsize=(16, 12))
 plt.barh(bulan, hasil, color='orange')
 
-plt.title("Report Montly Agustus 2025")
-plt.xlabel("rate(in unit)")
-plt.ylabel("Name")
+plt.title("Report Montly September 2025")
+plt.xlabel("Pending Amount Recovery(in unit)")
+plt.ylabel("Collector")
 
 plt.xlim(0,max(hasil)* 1.1)
 
-max_month = max(hasil)
-for riska, nurlita in enumerate(values):
-    if nurlita > 0:
-        plt.text(nurlita - (max_month*0.05), riska, f"{nurlita:.2f}", va='center', ha='left', color='black', fontsize=10, fontweight ='bold')
-        
+def format_number(riska):
+    return f"{riska:.2f}".rstrip('0').rstrip('.') if isinstance(riska, float) else str(riska)
 
-plt.subplots_adjust(left=0.2, right=0.95,bottom=0.05)
+max_month = max(hasil)
+for riska, nurlita in enumerate(hasil):
+    if riska > 0:
+        threshold = max_month * 0.08
+        label = format_number(nurlita)
+
+        if nurlita > threshold:
+            plt.text(nurlita - (max_month*0.05), riska, label, va='center', ha='left', color='black', fontsize=10, fontweight='bold')
+        else:
+            plt.text(nurlita + (max_month*0.05), riska, label, va='center', ha='left', color='black', fontsize=10, fontweight='bold')
+
+plt.subplots_adjust(left=0.25, right=0.95, bottom=0.05)
 plt.tight_layout()
 plt.savefig("grafik_Monthly_Agustus.png", dpi=100)
 plt.show()
@@ -144,3 +125,6 @@ average_result = total_average / len(Monthly)
 print()
 print(f"Average Team : {average_result:.2f} %")
 print("--------------------------------------------------------------------")
+
+    
+
