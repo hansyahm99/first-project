5bf133ab9cbe54d5258e472cb18a63cc3ee3572e
hansyahm99 <hansyahm99@gmail.com>
2025-09-12 15:20:12 +0700

    Update dashboard_s2.py

--- dashboard_s2.py
+++ dashboard_s2.py
@@ -73,7 +73,7 @@ pacthes, texts, autotexts = ax2.pie(rate, autopct='%1.2f%%', startangle=140, col
 ax2.set_title(f"Cycle S2 Recovery Rate (Target: 0.12)", fontsize= 7,  fontweight='bold')
 ax2.axis('equal')
 
-ax2.legend(pacthes, team, loc='center left', bbox_to_anchor=(0.5, -0.30), fontsize=6, ncol=3)
+ax2.legend(pacthes, team, loc='center left', bbox_to_anchor=(0.5, -0.10), fontsize=6, ncol=3)
 
 st.pyplot(fig2)
 st.dataframe(df_cycle[['Team', 'Recovery rate']])
