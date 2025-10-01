ae3540b66c1304f45d1d8da0b025adb1d7166cd3
hansyahm99 <hansyahm99@gmail.com>
2025-10-01 14:29:35 +0700

    Update dashboard_s2.py

--- dashboard_s2.py
+++ dashboard_s2.py
@@ -92,7 +92,7 @@ USER_CREDENTIALS = {
     "feronika": {"password": "12345", "files": {"daily": "riska.xlsx", "cycle": "riskuy.xlsx", "monthly": "nurlita.xlsx", "rank": "risnur.xlsx"}},
     "afrian": {"password": "12345", "files": {"daily": "riska.xlsx", "cycle": "riskuy.xlsx", "monthly": "nurlita.xlsx", "rank": "risnur.xlsx"}},
     "ayat": {"password": "12345", "files": {"daily": "riska.xlsx", "cycle": "riskuy.xlsx", "monthly": "nurlita.xlsx", "rank": "risnur.xlsx"}},
-    "nurani": {"password": "12345", "files": {"daily": "riska.xlsx", "cycle": "riskuy.xlsx", "monthly": "nurlita.xlsx", "rank": "risnur.xlsx"}},
+    "suwandi": {"password": "12345", "files": {"daily": "riska.xlsx", "cycle": "riskuy.xlsx", "monthly": "nurlita.xlsx", "rank": "risnur.xlsx"}},
 }
 CREDENTIALS_FILE = "user_credentials.json"
 
